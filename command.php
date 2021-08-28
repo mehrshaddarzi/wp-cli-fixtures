2f00b374dc905e7a47170428e78990b71d8b5a8a
mehrshaddarzi <mehrshad198@gmail.com>
2021-08-28 20:43:42 +0430

    Add Alice Library

--- command.php
+++ command.php
@@ -12,8 +12,8 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-if (file_exists(__DIR__ . '/alice/vendor/autoload.php')) {
-    require_once __DIR__ . '/alice/vendor/autoload.php';
+if (file_exists(__DIR__ . '/alice/lib/autoload.php')) {
+    require_once __DIR__ . '/alice/lib/autoload.php';
 }
 
 WP_CLI::add_command('fixtures', __NAMESPACE__ . '\\Command');
